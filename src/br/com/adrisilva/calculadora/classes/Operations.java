a9f03284c1bcb7877b44e28e1f69edc97a91f526
Adri Silva <30940624+AdriJhonson@users.noreply.github.com>
2018-02-12 13:35:07 -0300

    Add files via upload

---
+++ src/br/com/adrisilva/calculadora/classes/Operations.java
@@ -0,0 +1,20 @@
+package br.com.adrisilva.calculadora.classes;
+
+public class Operations {
+    public static float add(float value_one, float value_two){
+        return value_one + value_two;
+    }
+    
+    public static float subtract(float value_one, float value_two){
+        return value_one - value_two;
+    }
+    
+    public static float multiply(float value_one, float value_two){
+        return value_one * value_two;
+    }
+    
+    public static float divide(float value_one, float value_two){
+        return value_one / value_two;
+    }
+    
+}
